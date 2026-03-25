ce86072142856d12cff0b73f09382c20ae698fe7
Aurelie <3kdapp25.vaudau@folkuniversitetet.nu>
2026-03-25 20:58:46 +0100

    Initial Commit

---
+++ theMasterDetailGroupApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  theMasterDetailGroupApp
+//
+//  Created by Aurelie Vaudan APP25 on 2026-03-25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ theMasterDetailGroupApp/theMasterDetailGroupAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  theMasterDetailGroupAppApp.swift
+//  theMasterDetailGroupApp
+//
+//  Created by Aurelie Vaudan APP25 on 2026-03-25.
+//
+
+import SwiftUI
+
+@main
+struct theMasterDetailGroupAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
